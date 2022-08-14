3dcc83982ababb80b43119a69c8ce78ae8256064
skolastikarhea <skolastikarhea@gmail.com>
2022-08-14 14:40:53 +0700

    sample sitelist

--- sample/sitelist.php
+++ sample/sitelist.php
@@ -1,1 +1,10 @@
 <?php
+include_once 'api/api_aapanel_mitha.php';
+
+$aapanel = new aapanel_api;
+
+$aapanel->key = 'z6jcyJFMRgJCWNdyHelgi5ilrCbsHO19';
+$aapanel->url = 'http://192.168.2.12:7800';
+
+
+var_dump($aapanel->siteList(10, 1));
